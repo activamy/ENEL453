8e38914d1c8faa91ec9ebe600666a887a155a125
Julianne <julianne.generato@gmail.com>
2026-09-24 17:02:11 -0600

    Delete Lab1B directory

--- Lab1B/switch_logic.sv
+++
@@ -1,8 +0,0 @@
-module switch_logic (
-    input  logic [15:0] switches_inputs,
-    output logic [15:0] switches_outputs
-);
-
-    assign switches_outputs = switches_inputs; // switches_outputs also maps to the Basys3 LEDs in the .XDC file
-
-endmodule
